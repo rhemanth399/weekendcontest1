09f4414f97fb156933ffa842a3c6c7d625358ed9
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-07 14:19:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,47 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+  const developers = arr.map(employee => {
+    if (employee.profession === 'developer') {
+      return employee.name;
+    }
+  });
+  
+  console.log(developers);
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  const developers = [];
+  arr.forEach(employee => {
+    if (employee.profession === 'developer') {
+      developers.push(employee.name);
+    }
+  });
+
+  console.log(developers);
 }
 
 function addData() {
   //Write your code here
+  const newEmployee = { id: 4, name: 'Susan', age: 20, profession: 'intern' };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  const updatedEmployees = arr.filter(employee => employee.profession !== 'admin');
+  console.log(updatedEmployees);
 }
 
 function concatenateArray() {
   //Write your code here
+  const newEmployees = [
+    { id: 6, name: 'Bob', age: 27, profession: 'developer' },
+    { id: 7, name: 'Samantha', age: 22, profession: 'designer' },
+    { id: 8, name: 'Mike', age: 32, profession: 'manager' }
+  ];
+  const combinedArray = arr.concat(newEmployees);
+  console.log(combinedArray);
 }
